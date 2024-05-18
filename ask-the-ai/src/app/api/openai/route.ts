d9a883920b453c66e27b2349f00182be386539ba
sami <samiap914@gmail.com>
2024-05-18 19:46:30 +0530

    basic ai chatbot

--- ask-the-ai/src/app/api/openai/route.ts
+++ ask-the-ai/src/app/api/openai/route.ts
@@ -1,9 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 
 export async function POST(request: NextRequest) {
   try {
     const { query } = await request.json();
-    return NextResponse.json({ response: query });
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: query }],
+      model: "gpt-3.5-turbo",
+  });
+    return NextResponse.json({ response: chatCompletion.choices[0].message.content });
   } catch (error) {
     return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
   }
